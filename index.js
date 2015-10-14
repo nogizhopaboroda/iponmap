750196fcc65dcc336f944aa110afd7ff6d7af8ab
Alexander Zhukov <al.zhukoff@gmail.com>
2015-10-15 01:37:24 +0200

    version 1.0

--- index.js
+++ index.js
@@ -14,63 +14,69 @@ var ttys     = require('ttys');
 var geoip = require('geoip-lite');
 
 
-/**/
-var MAP_AR = 162 / 36; // 4.5
-var terminalAr = process.stdout.columns / process.stdout.rows;
+var map, log, screen;
+function drawApp(){
+  var MAP_AR = 162 / 36; // map aspect ratio, 4.5
+  var terminalAr = process.stdout.columns / process.stdout.rows; // terminal aspect ratio
 
-var matrix = 
-  terminalAr > MAP_AR
-  ? /* log on left */
-    /*   w      h     l      t */  
-    [
-      ["80%", "100%", 0    , 0    ],
-      ["20%", "100%", "80%", 0    ]
-    ]
-  : /* log on bottom */
-    [
-      ["100%", "80%", 0    , 0    ],
-      ["100%", "20%", 0    , "80%"]
-    ]
+  var matrix = 
+    terminalAr > MAP_AR
+      ? /* log on left */
+      /*   w      h     l      t */  
+      [
+        ["80%", "100%", 0    , 0      ], /* map */
+        ["20%", "100%", "80%", 0      ]  /* log */
+      ]
+        : /* log on bottom */
+        [
+          ["100%", "80%", 0    , 0    ],
+          ["100%", "20%", 0    , "80%"]
+        ]
 
-var screen = blessed.screen({
-  input: ttys.stdin, 
-  output: process.stdout
-});
+  screen = blessed.screen({
+    input: ttys.stdin, 
+    output: process.stdout
+  });
 
-var grid = new contrib.grid({rows: 2, cols: 2, screen: screen})
+  screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+    return process.exit(0);
+  });
 
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
- return process.exit(0);
-});
+  map = contrib.map({
+    label: 'Map',
+    width:  matrix[0][0],
+    height: matrix[0][1],
+    left:   matrix[0][2],
+    top:    matrix[0][3],
+    xPadding: 0,
+    border: {type: "line", fg: "cyan"}
+  });
+  screen.append(map);
 
-//var map = grid.set(0, 0, 2, 2, contrib.map, {label: 'Map'});
-var map = contrib.map({
-  label: 'Map',
-  width:  matrix[0][0],
-  height: matrix[0][1],
-  left:   matrix[0][2],
-  top:    matrix[0][3],
-  xPadding: 0,
-  border: {type: "line", fg: "cyan"}
-});
-screen.append(map);
+  log = contrib.log({ 
+    fg: "green" , 
+    selectedFg: "green" , 
+    label: "Log",
+    width:  matrix[1][0],
+    height: matrix[1][1],
+    left:   matrix[1][2],
+    top:    matrix[1][3],
+    xPadding: 0,
+    border: {type: "line", fg: "cyan"}
+  });
+  screen.append(log);
+}
+drawApp();
 
-//var log = grid.set(1, 0, 1, 2, contrib.log, { fg: "green" , selectedFg: "green" , label: 'Log'});
-var log = contrib.log({ 
-  fg: "green" , 
-  selectedFg: "green" , 
-  label: "Log",
-  width:  matrix[1][0],
-  height: matrix[1][1],
-  left:   matrix[1][2],
-  top:    matrix[1][3],
-  xPadding: 0,
-  border: {type: "line", fg: "cyan"}
-});
-log.log(process.stdout.columns + 'x' + process.stdout.rows);
-//console.log(process.stdout.columns + 'x' + process.stdout.rows);
-screen.append(log);
-/**/
+
+function drawMarkers(ipList){
+  ipList.forEach(function(ip){
+    var geo = geoip.lookup(ip);
+    map.addMarker({"lon" : "" + geo.ll[1], "lat" : "" + geo.ll[0], color: "red", char: "°" })
+    log.log(ip + " (" + (geo.city || "<unknown city>") + ", " + geo.country + ")");
+  });
+  screen.render();
+}
 
 
 var data = process.argv.slice(2);
@@ -90,22 +96,3 @@ if(data.length){
 
 
 
-function drawMarkers(ipList){
-
-  ipList.forEach(function(ip){
-    var geo = geoip.lookup(ip);
-    map.addMarker({"lon" : "" + geo.ll[1], "lat" : "" + geo.ll[0], color: "red", char: "·" })
-    log.log(ip + " (" + (geo.city || "<unknown city>") + ", " + geo.country + ")");
-  });
-
-  screen.render();
-
-}
-
-
-//  w        h   aspect ratio    matrix 
-//150-170 / 36 ~ 4                1x4
-//75-105 / 36  ~ 2                1x2
-//
-//
-// matrix formula: 1 / (apect ratio)  if AR >= 2
